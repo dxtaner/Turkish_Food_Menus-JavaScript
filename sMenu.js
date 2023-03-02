6bc5deba7ae43d78646ec3eca55f2e8c6a77ab18
Taner <taneozer16@gmail.com>
2023-03-02 22:50:49 +0300

    edited

--- sMenu.js
+++ sMenu.js
@@ -4,13 +4,13 @@ function initializePageFromMenu(menu) {
 }
 
 function displayMenuItems(menu, filter = "Tümü") {
-  const filterdMenu = menu.filter((menuItem) =>
-    filter == "Tümü" ? true : filter == menuItem.category
+  const filteredMenu = menu.filter((menuItem) =>
+    filter === "Tümü" ? true : filter === menuItem.category
   );
-  const menuItemElements = createMenuItemElements(filterdMenu);
+  const menuItemElements = createMenuItemElements(filteredMenu);
   const sectionCenter = document.querySelector(".section-center.row");
 
-  sectionCenter.innerHTML = " ";
+  sectionCenter.innerHTML = "";
 
   menuItemElements.forEach((menuItem) => {
     sectionCenter.appendChild(menuItem);
@@ -83,7 +83,7 @@ function createMenuItemImage(menuItem) {
 function createButtonsFromMenu(menu) {
   const buttonContainer = document.querySelector("div.btn-container");
 
-  const uniqueCategories =  getCategoriesFromMenu(menu);
+  const uniqueCategories = getCategoriesFromMenu(menu);
 
   uniqueCategories.forEach((category) => {
     const button = document.createElement("button");
@@ -171,6 +171,30 @@ const menu = [
   },
   {
     id: 7,
+    title: "Zeytinyağlı Kuru Fasulye",
+    category: "Zeytinyağlı Türk Yemekleri",
+    price: "15.00 tl",
+    img: "https://cdn.yemek.com/mncrop/940/625/uploads/2017/09/zeytinyagli-kuru-fasulye-tarifi-1.jpg",
+    desc: `Zeytinyağlı kuru fasulye, Türk mutfağının sevilen ve sık yapılan yemeklerinden biridir.Bol bol domates ve soğanla pişirilir ve genellikle pilavla birlikte servis edilir.`,
+  },
+  {
+    id: 8,
+    title: "Zeytinyağlı Barbunya Pilaki",
+    category: "Zeytinyağlı Türk Yemekleri",
+    price: "18.00 tl",
+    img: "https://i4.hurimg.com/i/hurriyet/75/750x422/5f87da37d3806c0b9cf1b3f3.jpg",
+    desc: `Zeytinyağlı barbunya pilaki, barbunyanın bol baharat ve zeytinyağıyla pişirilerek yapılan bir yemeğidir.Limonla servis edilir ve lezzeti çok beğenilir.`,
+  },
+  {
+    id: 9,
+    title: "Zeytinyağlı İmam Bayıldı",
+    category: "Zeytinyağlı Türk Yemekleri",
+    price: "20.00 tl",
+    img: "https://i4.hurimg.com/i/hurriyet/75/750x422/5ebe8c880f254414f09340c5.jpg",
+    desc: `Zeytinyağlı imam bayıldı, patlıcan ve soğanın bol baharat ve zeytinyağı ile birlikte pişirilmesiyle yapılan bir yemektir.İsmini, ilk kez bu yemeği yediğinde kendisinden geçen bir imamdan almıştır.`,
+  },
+  {
+    id: 10,
     title: "Lalanga",
     category: "Osmanlı Saray Mutfağı",
     price: "15.00 tl",
@@ -179,7 +203,7 @@ const menu = [
     denemeniz gereken bir tarif.`,
   },
   {
-    id: 8,
+    id: 11,
     title: "Biber Dolması",
     category: "Zeytinyağlı Türk Yemekleri",
     price: 12.99,
@@ -187,7 +211,7 @@ const menu = [
     desc: `Zeytinyağlı kontenjanının öne çıkan yemeği, kalabalık sofraların vazgeçilmezdir.`,
   },
   {
-    id: 9,
+    id: 12,
     title: "Karnıyarık",
     category: "Zeytinyağlı Türk Yemekleri",
     price: "13.99 tl",
@@ -196,7 +220,7 @@ const menu = [
      biber, domates ve kıyma da kullanılır.`,
   },
   {
-    id:10,
+    id: 13,
     title: "Keşkül Tatlısı",
     category: "Osmanlı Saray Mutfağı",
     price: "8.99 tl",
@@ -205,7 +229,7 @@ const menu = [
      Bu orijinal keşkül tarifinde örneğin un ya da nişasta yok; bolca badem var.`,
   },
   {
-    id: 11,
+    id: 14,
     title: "Meftune – Diyarbakır",
     category: "Yöresel Lezzetler",
     price: "19.25 tl",
@@ -213,13 +237,162 @@ const menu = [
     desc: `Patlıcan, biber, domatesler bolca sumak eşliğinde pişirilir. Etli ya da etkisi iki türlüsü de yapılır.`,
   },
   {
-    id: 12,
+    id: 15,
     title: "Bamya çorbası – Konya",
     category: "Yöresel Lezzetler",
     price: "22.25 tl",
     img: "https://cdn2.enuygun.com/media/lib/825x620/uploads/image/bamyacorbasi-35838.jpeg",
     desc: `Bamya çorbası. Klasik bamyadan farkı bamyaların çok küçük olması ve çorba gibi daha sulu yapılması.`,
   },
+  {
+    id: 16,
+    title: "Bursa İskender Kebabı",
+    category: "Yöresel Lezzetler",
+    price: "35.50 tl",
+    img: "https://cdn.yemek.com/mncrop/940/625/uploads/2014/09/iskender-kebap-tarifi-500x333.jpg",
+    desc: `Bursa'nın meşhur İskender kebabı, döner kebabının bir çeşididir. Etin üzerine tereyağlı domates sosu dökülerek servis edilir.`,
+  },
+  {
+    id: 17,
+    title: "Adana Kebap",
+    category: "Yöresel Lezzetler",
+    price: "28.00 tl",
+    img: "https://i.lezzet.com.tr/images-xxlarge-recipe/adana-kebabi-a95144f8-b2ee-4da1-b00a-a819010eb15f.jpg",
+    desc: `Adana'nın meşhur kebabı, baharatlı kıyma etinin şişe dizilmesiyle hazırlanır. Genellikle lavaş ekmeği, domates, biber ve soğanla servis edilir.`,
+  },
+  {
+    id: 18,
+    title: "İzmir Köfte",
+    category: "Yöresel Lezzetler",
+    price: "18.50 tl",
+    img: "https://cdn.yemek.com/mnresize/940/940/uploads/2017/08/izmir-kofte-tarifi.jpg",
+    desc: `İzmir'in meşhur köftesi, kıyma eti, soğan, sarımsak, maydanoz, yumurta ve baharatlarla hazırlanır. Genellikle domates, biber ve patatesle servis edilir.`,
+  },
+  {
+    id: 19,
+    title: "Antep Baklavası",
+    category: "Tatlılar",
+    price: "45.00 tl",
+    img: "https://cdn.yemek.com/mnresize/940/940/uploads/2014/12/baklava-tarifi.jpg",
+    desc: `Antep'in meşhur tatlısı, ince hamurla hazırlanan cevizli şerbetli bir tatlıdır. Üzerine Antep fıstığı serpilerek servis edilir.`,
+  },
+  {
+    id: 20,
+    title: "İstanbul Uskumru Köftesi",
+    category: "Yöresel Lezzetler",
+    price: "28.75 tl",
+    img: "https://cdn.yemek.com/mnresize/940/940/uploads/2015/06/uskumru-koftesi-tarifi.jpg",
+    desc: `İstanbul'un meşhur balık köftesi, uskumru balığı, patates, soğan, maydanoz, baharatlar ve galeta unuyla hazırlanır. Genellikle limon ve roka ile servis edilir.`,
+  },
+  {
+    id: 21,
+    title: "Hünkar Beğendi",
+    category: "Osmanlı Saray Mutfağı",
+    price: "35.50 tl",
+    img: "https://i.lezzet.com.tr/images-xxlarge-recipe/hunkar-begendi-afdf7bda-369c-4d8d-9d2a-62817ddc1403.jpg",
+    desc: `Osmanlı Sarayı'nın ünlü yemeklerinden Hünkar Beğendi, kuzu eti ile hazırlanan bir yemektir. Üzeri beşamel soslu patlıcan püresi ile kaplanır.`,
+  },
+  {
+    id: 22,
+    title: "Çerkez Tavuğu",
+    category: "Osmanlı Saray Mutfağı",
+    price: "32.00 tl",
+    img: "https://i.ytimg.com/vi/0oGzal8ldfs/maxresdefault.jpg",
+    desc: `Osmanlı Sarayı'nın özel yemeklerinden Çerkez Tavuğu, haşlanmış tavuk eti, ceviz, sarımsak, tahin ve baharatların karışımıyla hazırlanır. Genellikle maydanoz ve nar ekşisi ile servis edilir.`,
+  },
+  {
+    id: 23,
+    title: "Muhallebi",
+    category: "Osmanlı Saray Mutfağı",
+    price: "15.00 tl",
+    img: "https://i.ytimg.com/vi/KNYvEwpP41Y/maxresdefault.jpg",
+    desc: `Osmanlı Sarayı'nın en sevilen tatlılarından biri olan Muhallebi, süt, nişasta, şeker ve vanilya ile hazırlanan bir tür pudingdir. Genellikle üzerine tarçın serpilerek servis edilir.`,
+  },
+  {
+    id: 31,
+    title: "Adana Kebap",
+    category: "Türk Mutfağında Et Yemekleri",
+    price: "28.00 tl",
+    img: "https://i.ytimg.com/vi/Ld7JY1D8uSg/maxresdefault.jpg",
+    desc: `Adana Kebap, baharatlı kıyma etinin şişe takılarak mangalda pişirilmesiyle hazırlanan bir Türk mutfağı klasiğidir. Genellikle lavaş ekmeği, domates, biber ve soğanla servis edilir.`,
+  },
+  {
+    id: 32,
+    title: "İskender Kebap",
+    category: "Türk Mutfağında Et Yemekleri",
+    price: "30.00 tl",
+    img: "https://i.ytimg.com/vi/4Uzm-47pAV0/maxresdefault.jpg",
+    desc: `İskender Kebap, döner kebabın üzerine tereyağı ve domates sosu ile servis edilmesiyle hazırlanan bir yemektir. Genellikle yoğurtlu ve salçalı bir sosla servis edilir.`,
+  },
+  {
+    id: 33,
+    title: "Sivas Kangal Mantısı",
+    category: "Türk Mutfağında Et Yemekleri",
+    price: "25.00 tl",
+    img: "https://i.ytimg.com/vi/BmJtMbdmCSc/maxresdefault.jpg",
+    desc: `Sivas Kangal Mantısı, Türk mutfağının en özel mantı çeşitlerinden biridir. Küçük boyutta hazırlanan mantıların içi kıyma ve soğanla doldurulur ve üzerine yoğurt, tereyağı ve baharatlı sos dökülür.`,
+  },
+  {
+    id: 34,
+    title: "Kuzu Tandır",
+    category: "Türk Mutfağında Et Yemekleri",
+    price: "40.00 tl",
+    img: "https://i.ytimg.com/vi/QEZwRDQuTtQ/maxresdefault.jpg",
+    desc: `Kuzu tandır, tandır fırınında uzun saatler pişirilen kuzu etinin özel baharatlarla lezzetlendirilerek servis edilmesiyle hazırlanan bir yemektir. Genellikle pilav ve salata ile servis edilir.`,
+  },
+  {
+    id: 35,
+    title: "Ciğer Tava",
+    category: "Türk Mutfağında Et Yemekleri",
+    price: "22.00 tl",
+    img: "https://i.ytimg.com/vi/MbfyXYFg9nM/maxresdefault.jpg",
+    desc: `Ciğer Tava, tavada kızartılmış ciğerin soğan ve baharatlarla lezzetlendirilerek servis edilmesiyle hazırlanan bir yemektir. Genellikle sıcak olarak servis edilir.`,
+  },
+  {
+    id: 36,
+    title: "Baklava",
+    category: "Tatlılar",
+    price: "35.00 tl",
+    img: "https://i.ytimg.com/vi/33HT1jpMwI8/maxresdefault.jpg",
+    desc: `Baklava, yufkaların arasına ceviz veya fıstık gibi malzemelerin konulması ve şerbetlenmesiyle hazırlanan geleneksel bir Türk tatlısıdır. Çok katmanlı olması ve şerbetli olmasıyla öne çıkar.`,
+  },
+  {
+    id: 37,
+    title: "Sütlaç",
+    category: "Tatlılar",
+    price: "10.00 tl",
+    img: "https://i.ytimg.com/vi/x6XKXHKyHIs/maxresdefault.jpg",
+    desc: `Sütlaç, pirinç, süt, şeker ve vanilyanın pişirilerek hazırlanmasıyla yapılan bir Türk tatlısıdır. Üzerine tarçın serpilerek servis edilir.`,
+  },
+  {
+    id: 38,
+    title: "Künefe",
+    category: "Tatlılar",
+    price: "25.00 tl",
+    img: "https://i.ytimg.com/vi/NRdwOXAGvlg/maxresdefault.jpg",
+    desc: `Künefe, tel kadayıfın arasına peynir veya kaymak konulması ve şerbetlenmesiyle hazırlanan bir Türk tatlısıdır. Üzerine antep fıstığı serpilerek servis edilir.`,
+  },
+  {
+    id: 39,
+    title: "Revani",
+    category: "Tatlılar",
+    price: "15.00 tl",
+    img: "https://i.ytimg.com/vi/8_KvzKmvl-w/maxresdefault.jpg",
+    desc: `Revani, irmik, şeker, süt, yumurta ve limon kabuğunun karıştırılması ve fırında pişirilmesiyle hazırlanan bir Türk tatlısıdır. Üzerine şerbet dökülerek servis edilir.`,
+  },
+  {
+    id: 40,
+    title: "İrmik Helvası",
+    category: "Tatlılar",
+    price: "12.00 tl",
+    img: "https://i.ytimg.com/vi/_uX9bNluzdQ/maxresdefault.jpg",
+    desc: `İrmik Helvası, irmik, şeker, süt ve tereyağının kavrulmasıyla hazırlanan bir Türk tatlısıdır. Genellikle sıcak olarak servis edilir.`,
+  }
+  
+
+
+
+
 ];
 
 initializePageFromMenu(menu);
